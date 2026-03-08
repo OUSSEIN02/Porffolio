0af583475e8928a18ab0cbe902dd567cb1cd7e51
aloussein <alousseintchaboue@gmail.com>
2026-03-08 09:40:19 +0100

    first commit

---
+++ js/script.js
@@ -0,0 +1,8 @@
+/* ============ typing ============== */
+
+const typed = new Typed('.typing', {
+    strings:['', 'Développeur Front-end', 'Développeur Backend', ],
+    typeSpeed: 100,
+    BackSpeed: 60,
+    loop:true
+})
